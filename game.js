5e5c2641e172f04d64efcc4e394d8c7d6929be23
Guillaume Robert <guillaumerober@gmail.com>
2023-05-03 16:29:36 +0200

    destroy enemies !

--- game.js
+++ game.js
@@ -11,8 +11,8 @@ kaboom({
   const MOVE_SPEED = 50;
   const SLICER_SPEED = 100
   const SKELETOR_SPEED = 60;
-  const numberOfKeys = 0;
-  const numberOfBombs = 0;
+  let numberOfKeys = 0;
+  let numberOfBombs = 0;
   
 
   // Assets loading
@@ -34,6 +34,7 @@ kaboom({
   loadSprite('sword-right', 'sprites/sword-right.png')
   loadSprite('key', 'sprites/key.png')
   loadSprite('bomb', 'sprites/bomb.png')
+  loadSprite('boom', 'sprites/boom.png')
 
   
 
@@ -73,7 +74,7 @@ kaboom({
         'a': () => [sprite('block'), area(), solid()],
         '}': () => [sprite('skeleton'), 'skeleton', 'dangerous', area(), solid(), {dir: -1, timer: 0}],
        
-        'è': () => [sprite('slicer'), 'slicer', 'dangerous', area(), solid(), {dir: -1, timer: 0}],
+        'è': () => [sprite('slicer'), 'slicer', 'dangerous', area(), solid(), {dir: -1}],
         '|': () => [sprite("right-door")],
         '&': () => [sprite("left-statue"), area(), solid()],
         '@': () => [sprite("right-statue"), area(), solid() ],
@@ -123,9 +124,6 @@ kaboom({
       { dir: vec2(1, 0) },
     ]);
 
-    
-    
-  
     onKeyDown("left", () => {
       player.frame = 15;
       player.move(-MOVE_SPEED, 0);
@@ -150,6 +148,21 @@ kaboom({
       player.dir = vec2(0, 1);
     });
 
+    // Attaques 
+
+    function spawnKaboom(p) {
+      const obj = add([sprite('boom'), pos(p), area(),  'boom'])
+      wait(1, () => {
+        destroy(obj)
+      })
+    }
+
+    onKeyPress('space', () => {
+      spawnKaboom(player.pos.add(player.dir.scale(12)))
+    })
+
+    // Ennemis
+
     onUpdate('slicer', (s) => {
       
       s.move(s.dir * SLICER_SPEED, 0)
@@ -173,12 +186,20 @@ kaboom({
       });
     });
 
-    player.onCollide("bomb", () => {
-      
-      destroyAll("bomb");
-    })
+    player.onCollide("bomb", (b) => {
+      numberOfBombs.value++;
+      destroy(b);
+    });
 
-    
+    onCollide('boom', 'skeleton', (b,s) => {
+      
+      wait(1, () => {
+        destroy(b)
+      })
+      destroy(s)
+      scoreLabel.value++
+      scoreLabel.text = scoreLabel.value
+    })
 
     // die
     player.onCollide('dangerous', () => {
@@ -192,8 +213,8 @@ kaboom({
       pos(85, 100),
     ])
     add([ 
-      text("Press any key to start again"),
-      pos(15, 115),
+      text("Press any key to try again"),
+      pos(20, 115),
     ])
     onKeyPress(() => 	go("game", {
           level: 0, score: 0
