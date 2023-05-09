1907f586f1bdebb3e9fdc746e6ef40850e57b7db
Guillaume Robert <guillaumerober@gmail.com>
2023-05-09 13:32:56 +0200

    sword attack ok!

--- game.js
+++ game.js
@@ -1,339 +1,429 @@
 kaboom({
-    global: true,
-    width: 256,
-    height: 240,
-    font: "sinko",
-    scale: 3,
-    debug: true,
-    clearColor: [0, 0, 1, 1],
-  });
+  global: true,
+  width: 256,
+  height: 240,
+  font: "sinko",
+  scale: 3,
+  debug: true,
+  clearColor: [0, 0, 1, 1],
+});
 
-  const MOVE_SPEED = 50;
-  const SLICER_SPEED = 100
-  const SKELETOR_SPEED = 60;
-  const SNAKE_SPEED = 60;
-  let numKeys = 0;
-  let numBombs = 0;
-  
-  
+const MOVE_SPEED = 50;
+const SLICER_SPEED = 100
+const SKELETOR_SPEED = 60;
+const SNAKE_SPEED = 60;
+let numKeys = 0;
+let numBombs = 0;
 
-  // Assets loading
 
-  loadSprite("sheet", "sprites/AniaZelda-alt.png", {
-    sliceX: 4,
-    sliceY: 5,
-  });
 
-  
-  loadSprite('skeleton', 'sprites/skeleton.png');
-  loadSprite('block', 'sprites/block.png');
-  loadSprite('bg', 'sprites/bg.png')
-  loadSprite('locked-door1', 'sprites/locked-door1.png')
-  loadSprite('locked-door2', 'sprites/locked-door2.png')
-  loadSprite('left-statue', 'sprites/left-statue.png')
-  loadSprite('right-statue', 'sprites/right-statue.png')
-  loadSprite('downstairs', 'sprites/downstairs.png')
-  loadSprite('upstairs', 'sprites/upstairs.png')
-  loadSprite('slicer', 'sprites/slicer.png')
-  loadSprite('sword-right', 'sprites/sword-right.png')
-  loadSprite('key', 'sprites/key.png')
-  loadSprite('bomb', 'sprites/bomb.png')
-  loadSprite('boom', 'sprites/boom.png')
-  loadSprite('snake', 'sprites/snake.png')
+// Assets loading
 
-  
+loadSprite("sheet", "sprites/AniaZelda-alt.png", {
+  sliceX: 4,
+  sliceY: 5,
+});
 
-  scene("game", ({ level, score }) => {
-    layers(["bg", "obj", "ui"], "obj");
-    
+loadSprite("sword-sheet", "sprites/sword-sheet.png", {
+  sliceX: 4,
+  sliceY: 1,
+} )
 
-    // Maps 
+loadSprite('skeleton', 'sprites/skeleton.png');
+loadSprite('block', 'sprites/block.png');
+loadSprite('bg', 'sprites/bg.png')
+loadSprite('locked-door1', 'sprites/locked-door1.png')
+loadSprite('locked-door2', 'sprites/locked-door2.png')
+loadSprite('left-statue', 'sprites/left-statue.png')
+loadSprite('right-statue', 'sprites/right-statue.png')
+loadSprite('downstairs', 'sprites/downstairs.png')
+loadSprite('upstairs', 'sprites/upstairs.png')
+loadSprite('slicer', 'sprites/slicer.png')
+loadSprite('key', 'sprites/key.png')
+loadSprite('bomb', 'sprites/bomb.png')
+loadSprite('boom', 'sprites/boom.png')
+loadSprite('snake', 'sprites/snake.png')
+loadSprite('sword', 'sprites/sword.png')
 
-    const maps = [
+
+
+scene("game", ({ level, score }) => {
+  layers(["bg", "obj", "ui"], "obj");
+
+
+  // Maps 
+
+  const maps = [
     [
 
-    'aaaaaaaaaaaaaaaa',
-    'a  &        @  a',
-    'a    x         a',
-    'a              a',
-    'a  a      > a  a',
-    'a  a    }   a  a',
-    'a  a        a  |',
-    'a  a        a  )',
-    'a  a        a  a',
-    'a  a        a  a',
-    'a  a}       a  a',
-    'a              a',
-    'a          è   a',
-    'a  &       @   a',
-    'aaaaaaaaaaaaaaaa',
+      'aaaaaaaaaaaaaaaa',
+      'a  &        @  a',
+      'a    x         a',
+      'a              a',
+      'a  a      > a  a',
+      'a  a    }   a  a',
+      'a  a        a  |',
+      'a  a        a  )',
+      'a  a        a  a',
+      'a  a        a  a',
+      'a  a}       a  a',
+      'a              a',
+      'a          è   a',
+      'a  &       @   a',
+      'aaaaaaaaaaaaaaaa',
 
     ],
     [
 
-    'aaaaaaaaaaaaaaaa',
-    'a&            @a',
-    'a <            a',
-    'a              a',
-    'a  aaaaaaaaaa  a',
-    'a  a$       a  a',
-    'a  a        a  a',
-    'a  a   k    a  a',
-    'a  a        a  a',
-    'a  a       $a  a',
-    'a  aa      aa  a',
-    'a              a',
-    'a          è   a',
-    'a&            @a',
-    'aaaaaaaaaaaaaaaa',
-  
-      ],
-    ];  
-     
+      'aaaaaaaaaaaaaaaa',
+      'a&            @a',
+      'a <            a',
+      'a              a',
+      'a  aaaaaaaaaa  a',
+      'a  a$       a  a',
+      'a  a        a  a',
+      'a  a   k    a  a',
+      'a  a        a  a',
+      'a  a       $a  a',
+      'a  aa      aa  a',
+      'a              a',
+      'a          è   a',
+      'a&            @a',
+      'aaaaaaaaaaaaaaaa',
 
-  // Configuration des niveaux
-
-    const levelCfg = {
-        width: 16,
-        height: 16,
-        'a': () => [sprite('block'), 'wall', area(), solid()],
-        '}': () => [sprite('skeleton'), 'skeleton', 'dangerous', area(), solid(), {dir: -1, timer: 0}],
-        'x': () => [sprite('bomb'), area(), solid(), 'bomb'],
-        'k': () => [sprite('key'), area(), solid(), 'key'],        
-        'è': () => [sprite('slicer'), 'slicer', 'dangerous', area(), {dir: -1}],
-        '$': () => [sprite('snake'), 'snake', 'dangerous', area(), {dir: -1}],
-        '|': () => [sprite("locked-door1"), area(), solid()],
-        ')': () => [sprite("locked-door2")],
-        '&': () => [sprite("left-statue"), area(), solid()],
-        '@': () => [sprite("right-statue"), area(), solid() ],
-        '<': () => [sprite("upstairs"), area(), 'previous-level' ],
-        '>': () => [sprite("downstairs"), area(), 'next-level'],
-        
-
-    }
-    addLevel(maps[level], levelCfg);
-    
-    // UI
-
-    const scoreLabel = add([
-      text("0"),
-      pos(200, 180),
-      layer("ui"),
-      { value: score },
-      // { value: 2 },
-      scale(0.7),
-    ]);
-
-    const keysLabel = add([
-      text("Keys : "),
-      pos(200, 200),
-      layer("ui"),
-      { value: score },
-      // { value: 2 },
-      scale(0.7),
-    ]);
-  
-    const levelLabel = add([
-      text("Room " + parseInt(level + 1)), //
-      pos(200, 215),
-      scale(0.7),
-    ]);
-
-    const bombsLabel = add([
-      text("Bombs : "), //
-      pos(200, 230),
-      layer("ui"),
-      scale(0.7),
-    ]);
-
-    
-    // Fond
-
-    add([sprite('bg'), layer('bg')])
-     
-
-    // Joueur 
-
-    const player = add([
-      sprite("sheet", { frame: 14 }), //
-      pos(30, 60),
-      area(),
-          solid(),
-      { dir: vec2(1, 0) },
-    ]);
-
-    onKeyDown("left", () => {
-      player.frame = 15;
-      player.move(-MOVE_SPEED, 0);
-      player.dir = vec2(-1, 0);
-    });
-  
-    onKeyDown("right", () => {
-      player.frame = 14;
-      player.move(MOVE_SPEED, 0);
-      player.dir = vec2(1, 0);
-    });
-  
-    onKeyDown("up", () => {
-      player.frame = 13;
-      player.move(0, -MOVE_SPEED);
-      player.dir = vec2(0, -1);
-    });
-  
-    onKeyDown("down", () => {
-      player.frame = 12;
-      player.move(0, MOVE_SPEED);
-      player.dir = vec2(0, 1);
-    });
-
-    // Attaques 
-
-    function spawnKaboom(p) {
-      const obj = add([sprite('boom'), pos(p), area(),  'boom'])
-      wait(1, () => {
-        destroy(obj)
-      })
-    }
-
-    onKeyPress('space', () => {
-      spawnKaboom(player.pos.add(player.dir.scale(12)))
-    })
-
-    // Ramasser objets 
-
-    function addKey() {
-      numKeys++;
-      keysLabel.text = 'Keys : ${numKeys}';
-    }
-
-    function addBomb() {
-      numKeys++;
-      bombsLabel.text = 'Bombs : ${numBombs}';
-    }
-
-    function removeKey() {
-      numKeys--;
-      keysLabel.text = 'Keys : ${numKeys}';
-    }
-
-    function removeBomb() {
-      numBombs--;
-      bombsLabel.text = 'Bombs : ${numBombs}';
-    }
-
-    // Ouvrir porte 
+    ],
+  ];
 
 
+  // Level config
 
-    // Ennemis
+  const levelCfg = {
+    width: 16,
+    height: 16,
+    'a': () => [sprite('block'), 'wall', area(), solid()],
+    '}': () => [sprite('skeleton'), 'skeleton', 'dangerous', area(), solid(), { dir: -1, timer: 0 }],
+    'x': () => [sprite('bomb'), area(), solid(), 'bomb'],
+    'k': () => [sprite('key'), area(), solid(), 'key'],
+    'è': () => [sprite('slicer'), 'slicer', 'dangerous', area(), { dir: -1 }],
+    '$': () => [sprite('snake'), 'snake', 'dangerous', area(), { dir: -1 }],
+    '|': () => [sprite("locked-door1"), 'locked-door1', area()],
+    ')': () => [sprite("locked-door2")],
+    '&': () => [sprite("left-statue"), area(), solid()],
+    '@': () => [sprite("right-statue"), area(), solid()],
+    '<': () => [sprite("upstairs"), area(), 'previous-level'],
+    '>': () => [sprite("downstairs"), area(), 'next-level'],
 
-    onUpdate('slicer', (s) => {
-      
-      s.move(s.dir * SLICER_SPEED, 0)
-    });
 
-    onCollide('slicer', 'wall', (s) => {
-      s.dir = -s.dir
-    })
+  }
+  addLevel(maps[level], levelCfg);
 
-    onUpdate('snake', (s) => {
-      
-      s.move(s.dir * SNAKE_SPEED, 0)
-    });
+  // UI
 
-    onCollide('snake', 'wall', (s) => {
-      s.dir = -s.dir
-    })
+  const scoreLabel = add([
+    text("0"),
+    pos(200, 180),
+    layer("ui"),
+    { value: score },
+    // { value: 2 },
+    scale(0.7),
+  ]);
 
-    onUpdate('skeleton', (s) => {
-      s.move(0, s.dir * SKELETOR_SPEED)
-      s.timer -= dt()
-      if (s.timer <= 0) {
-        s.dir = -s.dir
-        s.timer = rand(5)
-      }
-    });
+  const keysLabel = add([
+    text("Keys : "),
+    pos(200, 200),
+    layer("ui"),
+    { value: numKeys },
+    // { value: 2 },
+    scale(0.7),
+  ]);
 
-    player.onCollide('locked-door1', (l) => {
-      
-      if(numKeys = 0) {
-        add([
-          text('You need a key to open that door'),
-          pos(20, 200)
-        ])
-      }
-    })
+  const levelLabel = add([
+    text("Room " + parseInt(level + 1)), //
+    pos(200, 215),
+    scale(0.7),
+  ]);
 
-    // Collisions
+  const bombsLabel = add([
+    text("Bombs : "), //
+    pos(200, 230),
+    layer("ui"),
+    { value: numBombs },
+    scale(0.7),
+  ]);
 
-    player.onCollide('next-level', () => {
-      go("game", {
-        level: (level + 1),
-        score: scoreLabel.value,
-      });
-    });
 
-    player.onCollide('previous-level', () => {
-      go("game", {
-        level: (level - 1),
-        score: scoreLabel.value,
-      });
-    });
+  // Background
 
-    // Ramasser objets
-    player.onCollide('bomb', (b) => {
-      addBomb();
-      destroy(b);
-      
-      
-    })
+  add([sprite('bg'), layer('bg')])
 
-    player.onCollide('key', (k) => {
-      addKey();
-      destroy(k)
-      
-      
-    })
-    // Détruire ennemis
-    onCollide('boom', 'skeleton', (b,s) => {
-      
-      wait(1, () => {
-        destroy(b)
-      })
-      destroy(s)
-      scoreLabel.value++
-      scoreLabel.text = scoreLabel.value
-    })
 
-    onCollide('boom', 'snake', (b,s) => {
-      
-      wait(1, () => {
-        destroy(b)
-      })
-      destroy(s)
-      scoreLabel.value++
-      scoreLabel.text = scoreLabel.value
-    })
+  // Player 
 
-    // die
-    player.onCollide('dangerous', () => {
-      go('lose');
-    });
-  }) 
+  const player = add([
+    sprite("sheet", { frame: 14 }), //
+    pos(30, 60),
+    area(),
+    solid(),
+    { dir: vec2(1, 0) },
+  ]);
 
-  scene("lose", () => {
-    add([ 
-      text("Game Over"),
-      pos(85, 100),
-    ])
-    add([ 
-      text("Press any key to try again"),
-      pos(20, 115),
-    ])
-    onKeyPress(() => 	go("game", {
-          level: 0, score: 0
-        }));
+  onKeyDown("left", () => {
+    player.frame = 15;
+    player.move(-MOVE_SPEED, 0);
+    player.dir = vec2(-1, 0);
   });
+
+  onKeyDown("right", () => {
+    player.frame = 14;
+    player.move(MOVE_SPEED, 0);
+    player.dir = vec2(1, 0);
+  });
+
+  onKeyDown("up", () => {
+    player.frame = 13;
+    player.move(0, -MOVE_SPEED);
+    player.dir = vec2(0, -1);
+  });
+
+  onKeyDown("down", () => {
+    player.frame = 12;
+    player.move(0, MOVE_SPEED);
+    player.dir = vec2(0, 1);
+  });
+
+  // Attacks 
+
+  let attacking = false; 
+  let attackDir;
+
+  function attack() {
+    const playerPos = player.pos;
+  
+    const swordFrames = add([
+      sprite("sword-sheet", { frame: 0 }),
+      'sword',
+      pos(playerPos),
+      area(),
+    ]);
+  
+    attacking = true;
+  
+    if (player.frame === 13) {
+      attackDir = vec2(0, -1);
+      swordFrames.frame = 0;
+      swordFrames.pos.y -= 12;
+    } else if (player.frame === 12) {
+      attackDir = vec2(0, 1);
+      swordFrames.frame = 1;
+      swordFrames.pos.y += 12;
+    } else if (player.frame === 15) {
+      attackDir = vec2(-1, 0);
+      swordFrames.frame = 2;
+      swordFrames.pos.x -= 12;
+    } else if (player.frame === 14) {
+      attackDir = vec2(1, 0);
+      swordFrames.frame = 3;
+      swordFrames.pos.x += 12;
+    }
+  
+    wait(0.2, () => {
+      attacking = false;
+      if (player.frame === 13) {
+        player.frame = 13; // Revenir à la frame 13 (regardant vers le haut)
+      } else if (player.frame === 12) {
+        player.frame = 12; // Revenir à la frame 12 (regardant vers le bas)
+      } else {
+        player.frame = player.dir.x === 1 ? 14 : 15;
+      }
+    });
+  
+    wait(0.2, () => {
+      destroy(swordFrames);
+    });
+  }
   
   
-  go("game", { level: 0, score: 0 });
+
+  onKeyPress('space', () => { // bind to the space key
+  if (!attacking) { // only attack if not already attacking
+    attack(); 
+  }
+})
+
+  
+ 
+  /*function spawnKaboom(p) {
+    const obj = add([sprite('boom'), pos(p), area(), 'boom'])
+    wait(1, () => {
+      destroy(obj)
+    })
+  }
+
+  onKeyPress('space', () => {
+    spawnKaboom(player.pos.add(player.dir.scale(12)))
+  })*/
+
+  // Picking items
+
+  function addKey() {
+    numKeys++;
+    keysLabel.text = 'Keys : ' + numKeys;
+
+  }
+
+  function addBomb() {
+    numBombs++;
+    bombsLabel.text = 'Bombs : ' + numBombs;
+
+  }
+
+  function removeKey() {
+    numKeys--;
+    keysLabel.text = 'Keys : ' + numKeys;
+  }
+
+  function removeBomb() {
+    numBombs--;
+    bombsLabel.text = 'Bombs : ' + numBombs;
+  }
+
+  // Open door 
+
+  player.onCollide('locked-door1', () => {
+
+    if (numKeys = 0) {
+      add([
+        text('You need a key to open that door'),
+        pos(20, 200)
+      ])
+    }
+  })
+
+  // Enemies
+
+  onUpdate('slicer', (s) => {
+
+    s.move(s.dir * SLICER_SPEED, 0)
+  });
+
+  onCollide('slicer', 'wall', (s) => {
+    s.dir = -s.dir
+  })
+
+  onUpdate('snake', (s) => {
+
+    s.move(s.dir * SNAKE_SPEED, 0)
+  });
+
+  onCollide('snake', 'wall', (s) => {
+    s.dir = -s.dir
+  })
+
+  onUpdate('skeleton', (s) => {
+    s.move(0, s.dir * SKELETOR_SPEED)
+    s.timer -= dt()
+    if (s.timer <= 0) {
+      s.dir = -s.dir
+      s.timer = rand(5)
+    }
+  });
+
+
+
+  // Collisions
+
+  player.onCollide('next-level', () => {
+    go("game", {
+      level: (level + 1),
+      score: scoreLabel.value,
+      numBombs: bombsLabel.value,
+      numKeys: keysLabel.value,
+    });
+  });
+
+  player.onCollide('previous-level', () => {
+    go("game", {
+      level: (level - 1),
+      score: scoreLabel.value,
+      numBombs: bombsLabel.value,
+      numKeys: keysLabel.value,
+    });
+  });
+
+  // Ramasser objets
+  player.onCollide('bomb', (b) => {
+    addBomb();
+    destroy(b);
+
+
+  })
+
+  player.onCollide('key', (k) => {
+    addKey();
+    destroy(k)
+
+
+  })
+  // Détruire ennemis
+  /*onCollide('boom', 'skeleton', (b, s) => {
+
+    wait(1, () => {
+      destroy(b)
+    })
+    destroy(s)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })*/
+
+  onCollide('sword', 'skeleton', (w, s) => {
+
+    wait(1, () => {
+      destroy(w)
+    })
+    destroy(s)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })
+
+  /*onCollide('boom', 'snake', (b, s) => {
+
+    wait(1, () => {
+      destroy(b)
+    })
+    destroy(s)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })*/
+
+  onCollide('sword', 'snake', (w, s) => {
+
+    wait(1, () => {
+      destroy(w)
+    })
+    destroy(s)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })
+
+  // die
+  player.onCollide('dangerous', () => {
+    go('lose');
+  });
+})
+
+scene("lose", () => {
+  add([
+    text("Game Over"),
+    pos(85, 100),
+  ])
+  add([
+    text("Press any key to try again"),
+    pos(20, 115),
+  ])
+  onKeyPress(() => go("game", {
+    level: 0, score: 0
+  }));
+});
+
+
+go("game", { level: 0, score: 0, numBombs: 0, numKeys: 0 });
